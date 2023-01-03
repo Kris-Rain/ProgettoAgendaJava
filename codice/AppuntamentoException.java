37c0f4dfdb620c12a3d0d18e4dee1ed492d3b83b
Kris <82761258+Kris-Rain@users.noreply.github.com>
2023-01-03 20:25:35 +0100

    Create AppuntamentoException.java

---
+++ codice/AppuntamentoException.java
@@ -0,0 +1,8 @@
+package codice;
+
+public class AppuntamentoException extends Exception{
+	private static final long serialVersionUID = 1L;
+	public AppuntamentoException(String messaggio) {
+		super(messaggio);
+	}
+}
