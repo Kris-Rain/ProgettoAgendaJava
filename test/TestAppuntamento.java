cca4503a32ae05e8d8d27bf022b11d0e785d1491
Nicolò Bianchetto <121885541+NicoWh0@users.noreply.github.com>
2023-01-04 19:33:01 +0100

    Update TestAppuntamento.java

--- test/TestAppuntamento.java
+++ test/TestAppuntamento.java
@@ -15,12 +15,24 @@ class TestAppuntamento {
 
 	@Test
 	void testCostruttoreCompleto() throws AppuntamentoException {
-		String data="10-12-2023";
-		String orario="19:59";
+		String data = "10-12-2023";
+		String orario = "19:59";
 		assertDoesNotThrow(() -> Controllo.controlloData(data));
 		assertDoesNotThrow(() -> Controllo.controlloOrario(orario));
-		Appuntamento a1=new Appuntamento("10-12-2021", "13-30", "30", "Milano", "Luca");
+		Appuntamento a1 = new Appuntamento("10-12-2021", "13-30", "30", "Milano", "Luca");
 		assertEquals("10-12-2021", a1.getData());
-		assertEquals("Luca", a1.getNome());
+		assertFalse(a1.matchPersone("Luca", "Giacomo"));
+	}
+	
+	
+	@Test
+	void testSommaOrario() {
+		assertEquals("14:00", new Appuntamento("10-12-2021", "13-30", "30", "Milano", "Luca").getOrarioFine());
+		assertEquals("14:15", new Appuntamento("10-12-2021", "13-30", "45", "Milano", "Luca").getOrarioFine());
+		assertEquals("14:30", new Appuntamento("10-12-2021", "13-30", "60", "Milano", "Luca").getOrarioFine());
+		assertEquals("13:30", new Appuntamento("10-12-2021", "13-30", "0", "Milano", "Luca").getOrarioFine());
+		assertEquals("13:32", new Appuntamento("10-12-2021", "13-30", "2", "Milano", "Luca").getOrarioFine());
+		assertEquals("16:00", new Appuntamento("10-12-2021", "13-30", "150", "Milano", "Luca").getOrarioFine());
+		
 	}
 }
