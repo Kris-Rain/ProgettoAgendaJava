e380d5b7e62cbf3822eabe33edc4d953e0ea2fd5
Nicolò Bianchetto <121885541+NicoWh0@users.noreply.github.com>
2023-01-05 14:06:30 +0100

    Update DataOrario.java

--- src/codice/DataOrario.java
+++ src/codice/DataOrario.java
@@ -1,25 +1,75 @@
 package codice;
 
 import java.time.*;
+import java.time.format.DateTimeFormatter;
 
 public class DataOrario {
 	
 	private LocalDate data;
 	private LocalTime orario;
+	private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-uuuu");
 	
-	public DataOrario() {	
-		data = LocalDate.now();
-		orario = LocalTime.now();
+	
+	public DataOrario(LocalDate data, LocalTime orario) {
+		this.data = data;
+		this.orario = orario;
 	}
 	
 	public DataOrario(String data, String orario) {
-		this.data = LocalDate.parse(data);
-		this.orario = LocalTime.parse(orario);
+		this(LocalDate.parse(data, formatter), LocalTime.parse(orario));
 	}
 	
-	public void addTempo(String durata) {
+	public DataOrario() {	
+		this(LocalDate.now(), LocalTime.now());
+	}
+
+	
+	public LocalDate getData() {
+		return data;
+	}
+	
+	public LocalTime getOrario() {
+		return orario;
+	}
+	
+	
+	public String getDataToString() {
+		return data.format(formatter);
+	}
+	
+	public String getOrarioToString() {
+		return orario.toString();
+	}
+	
+	public void addTempoMinuti(String durata) {
 		LocalDateTime ldt = data.atTime(orario).plusMinutes(Long.parseLong(durata));
-		data.adjustInto(ldt);
-		orario.adjustInto(ldt);
+		data = ldt.toLocalDate();
+		orario = ldt.toLocalTime();
+	}
+	
+	public DataOrario plusMinuti(String durata) {
+		LocalDateTime ldt = data.atTime(orario).plusMinutes(Long.parseLong(durata));
+		return new DataOrario(ldt.toLocalDate(), ldt.toLocalTime());
+	}
+	
+	
+	public int compareTo(DataOrario other) {
+		LocalDateTime thisDateTime = data.atTime(orario), otherDateTime = other.getData().atTime(other.getOrario());
+		return thisDateTime.compareTo(otherDateTime);
+	}
+	
+	
+	public int compareTo(String data, String orario) {
+		return compareTo(new DataOrario(data, orario));
+	}
+	
+	
+	public int compareTo(LocalDate data, LocalTime orario) {
+		return compareTo(new DataOrario(data, orario));
+	}
+	
+	@Override
+	public String toString() {
+		return data.format(formatter) + " " + getOrarioToString();
 	}
 }
