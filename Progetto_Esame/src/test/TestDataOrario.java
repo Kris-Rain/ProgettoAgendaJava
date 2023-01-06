f08be67692326f7b01dc5985a3cf411ba4bb9875
Kris <82761258+Kris-Rain@users.noreply.github.com>
2023-01-06 20:20:31 +0100

    Update TestDataOrario.java

--- Progetto_Esame/src/test/TestDataOrario.java
+++ Progetto_Esame/src/test/TestDataOrario.java
@@ -1,1 +1,41 @@
+package test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.Test;
+import codice.DataOrario;
+
+class TestDataOrario {
+
+
+	@Test
+	void testCostruttore() {
+		DataOrario datatime = new DataOrario("12-02-2022", "12:31");
+		assertEquals("12-02-2022", datatime.getDataToString());
+		assertEquals("12:31", datatime.getOrarioToString());
+	}
+	
+	@Test
+	void testAddTempo() {
+		DataOrario datatime = new DataOrario("29-02-2020", "00:11");
+		datatime.addTempoMinuti("150");
+		assertEquals("29-02-2020 02:41", datatime.toString());
+		datatime.addTempoMinuti("1440");
+		assertEquals("01-03-2020 02:41", datatime.toString());
+		assertEquals("01-01-2023 00:30", new DataOrario("31-12-2022", "23:30").plusMinuti("60").toString());
+		assertEquals("01-03-2001 00:30", new DataOrario("28-02-2001", "23:30").plusMinuti("60").toString());
+	}
+	
+	
+	@Test
+	void testComparazione() {
+		assertEquals(-1, new DataOrario("28-02-2001", "23:30").compareTo(new DataOrario("01-03-2001", "00:30")));
+		assertEquals(1, new DataOrario("28-02-2001", "00:00").compareTo(new DataOrario("27-02-2001", "23:59")));
+		assertEquals(0, new DataOrario("28-02-2001", "00:00").compareTo(new DataOrario("28-02-2001", "00:00")));
+		assertEquals(-1, new DataOrario("28-02-2001", "00:00").compareTo(new DataOrario("28-02-2001", "00:01")));
+		assertEquals(-1, new DataOrario("31-12-2020", "23:59").compareTo(new DataOrario("01-01-2021", "00:00")));
+		assertEquals(1, new DataOrario("01-01-2021", "00:00").compareTo(new DataOrario("31-12-2020", "23:59")));
+	}
+
+}
