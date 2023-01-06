20456e2a50c9242ef5ac71b81d83901b76fb92ea
Kris <82761258+Kris-Rain@users.noreply.github.com>
2023-01-06 20:09:17 +0100

    Update TestAppuntamento.java

--- src/test/TestAppuntamento.java
+++ src/test/TestAppuntamento.java
@@ -16,7 +16,7 @@ class TestAppuntamento {
 	@Test
 	void testCostruttoreCompleto() throws AppuntamentoException {
 		String data = "10-12-2024";
-		String orario = "19:59";
+		String orario = "19-59";
 		assertDoesNotThrow(() -> Controllo.controlloData(data));
 		assertDoesNotThrow(() -> Controllo.controlloData("29-02-2024"));
 		assertThrows(AppuntamentoException.class, () -> Controllo.controlloData("29-02-2023"));
@@ -25,18 +25,30 @@ class TestAppuntamento {
 		assertDoesNotThrow(() -> Controllo.controlloOrario(orario));
 		Appuntamento a1 = new Appuntamento("10-12-2021", "13-30", "30", "Milano", "Luca");
 		assertEquals("10-12-2021", a1.getData());
-		assertFalse(a1.matchPersone("Luca", "Giacomo"));
+		assertFalse(a1.matchPersona("Giacomo"));
 	}
 	
+	@Test
+	void testIsCompatible() {
+		Appuntamento a1 = new Appuntamento("10-12-2021", "23-30", "30", "Milano", "Luca");
+		Appuntamento a2 = new Appuntamento("10-12-2021", "23-45", "30", "Milano", "Luca");
+		Appuntamento a3 = new Appuntamento("11-12-2021", "13-30", "30", "Milano", "Luca");
+		Appuntamento a4 = new Appuntamento("10-12-2021", "14-00", "30", "Milano", "Luca");
+		Appuntamento a5 = new Appuntamento("10-12-2021", "13-45", "30", "Milano", "Luca");
+		assertFalse(a1.isCompatible(a2));
+		assertTrue(a1.isCompatible(a3));
+		assertTrue(a1.isCompatible(a4));
+		assertTrue(a1.isCompatible(a5));
+	}
 	
 	@Test
-	void testSommaOrario() {
-		assertEquals("14:00", new Appuntamento("10-12-2021", "13-30", "30", "Milano", "Luca").getOrarioFine());
-		assertEquals("14:15", new Appuntamento("10-12-2021", "13-30", "45", "Milano", "Luca").getOrarioFine());
-		assertEquals("14:30", new Appuntamento("10-12-2021", "13-30", "60", "Milano", "Luca").getOrarioFine());
-		assertEquals("13:30", new Appuntamento("10-12-2021", "13-30", "0", "Milano", "Luca").getOrarioFine());
-		assertEquals("13:32", new Appuntamento("10-12-2021", "13-30", "2", "Milano", "Luca").getOrarioFine());
-		assertEquals("16:00", new Appuntamento("10-12-2021", "13-30", "150", "Milano", "Luca").getOrarioFine());
-		
+	void testMatchSingoloNome() {
+		assertTrue(new Appuntamento("10-12-2021", "23-30", "30", "Milano", "Luca").matchPersona("LUCA"));
+		assertTrue(new Appuntamento("10-12-2021", "23-30", "30", "Milano", "Luca").matchPersona("luca"));
+		assertTrue(new Appuntamento("10-12-2021", "23-30", "30", "Milano", "Luca").matchPersona("Luca"));
+		assertTrue(new Appuntamento("10-12-2021", "23-30", "30", "Milano", "Luca").matchPersona("lUca"));
+		assertTrue(new Appuntamento("10-12-2021", "23-30", "30", "Milano", "Luca").matchPersona("luCA"));
+		assertTrue(new Appuntamento("10-12-2021", "23-30", "30", "Milano", "Luca").matchPersona("LUca"));
+
 	}
 }
