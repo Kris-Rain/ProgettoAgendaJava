6c6f5032a4fecf2c99805c8b6ad65ccbb0a4725f
Nicolò Bianchetto <121885541+NicoWh0@users.noreply.github.com>
2023-01-22 17:28:31 +0100

    Update Interfaccia.java

--- Progetto_Esame/src/interfaccia/Interfaccia.java
+++ Progetto_Esame/src/interfaccia/Interfaccia.java
@@ -1,8 +1,3 @@
-/**
- * @author Nicolò Bianchetto
- * @author Kristian Rigo
- */
-
 package interfaccia;
 
 import java.io.File;
@@ -15,11 +10,29 @@ import jbook.util.*;
 import codice.Appuntamento.ControlloDati;
 import codice.Appuntamento.ControlloDati.TipoControllo;
 
+/**
+ * Semplice interfaccia testuale (di dimostrazione) per l'uso delle classi
+ * {@link Agenda}, {@link Appuntamento}, {@link ContenitoreAgende}.
+ * Il fine di questa interfaccia è quello di elencare, interagendo con l'utente, i principali
+ * aspetti e funzioni delle classi sopracitate.
+ * <p><em>Questa classe fa anche uso di {@link RequestApp} per facilitarne la stesura del codice.</em>
+ * 
+ * @see Agenda
+ * @see Appuntamento
+ * @see ContenitoreAgende
+ * @see RequestApp
+ * 
+ * 
+ * @author Nicolò Bianchetto
+ * @author Kristian Rigo
+ */
+
+
 public class Interfaccia {
 	private static final String ANSI_RESET = "\u001B[0m";
 	private static final String ANSI_GREEN = "\u001B[32m";
 	private static final String ANSI_YELLOW = "\u001B[33m";
-	public static final String YELLOW_BRIGHT = "\033[0;93m";
+	private static final String YELLOW_BRIGHT = "\033[0;93m";
 	
     private static ContenitoreAgende box = new ContenitoreAgende();
 
@@ -172,7 +185,7 @@ public class Interfaccia {
     	requests.put(TipoControllo.CONTROLLO_NOME, new RequestApp("Inserire il nome della persona con cui si ha l'appuntamento (ammessi solo caratteri alfanumerici, ma NON SOLO numeri): ", "Il nome non è valido!"));
     	int index = 0;
     	for(TipoControllo tc : TipoControllo.values()) {
-    		parametri[index++] = controlloDatoAppuntamento(tc, requests.get(tc).getRequest(), requests.get(tc).getErrMessage());
+    		parametri[index++] = controlloDatoAppuntamento(tc, requests.get(tc).REQUEST, requests.get(tc).ERR_MESSAGE);
     	}
     	if(agenda.aggiungiAppuntamento(parametri[0], parametri[1], parametri[2], parametri[3], parametri[4])) System.out.println(ANSI_GREEN+"\n*** Appuntamento aggiunto con successo ***"+ANSI_RESET);
     	else System.out.println(ANSI_YELLOW+"\nATTENZIONE: Appuntamento non compatibile con l'agenda"+ANSI_RESET);
