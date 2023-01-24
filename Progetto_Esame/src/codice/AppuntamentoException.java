bc1a5c6468dfffc0afd367d1fafe1286d347c69f
Kris <82761258+Kris-Rain@users.noreply.github.com>
2023-01-24 22:34:24 +0100

    Update AppuntamentoException.java

--- Progetto_Esame/src/codice/AppuntamentoException.java
+++ Progetto_Esame/src/codice/AppuntamentoException.java
@@ -1,11 +1,14 @@
 package codice;
 
 /**
- * La classe {@code AppuntamentoException} è una forma di {@code Throwable}
- * che indica le condizioni che si potrebbe voler catturare.
+ * La classe {@code AppuntamentoException} è una forma di {@code Throwable} che estende {@link Exception}
+ * e che indica le condizioni che si potrebbe voler catturare.<br>
  * Le eccezioni controllate devono essere dichiarate in una clausola {@code throws}
- * di un metodo o di un costruttore se possono essere generate dall'esecuzione 
- * del metodo o del costruttore.
+ * di un metodo o di un costruttore se possono essere generate dall'esecuzione
+ * del metodo o del costruttore.<p>
+ * Indica il tentativo di creare un {@link Appuntamento} con parametri errati, in quel caso verrà sollevata
+ * l'eccezione {@code AppuntamentoException} che potrà essere gestita tramite una {@code catch}
+ * utilizzando il costruttore {@link #AppuntamentoException} e passando un messaggio come argomento.
  * 
  * @author Kristian Rigo
  * @author Nicolò Bianchetto
@@ -13,6 +16,14 @@ package codice;
 
 public class AppuntamentoException extends Exception{
 	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Costruttore per {@code AppuntamentoException} che prende un messaggio di tipo {@link String}
+	 * come argomento e lo restituisce alla classe {@code super}.
+	 * 
+	 * @param messaggio da passare alla classe {@code super}.
+	 */
+	
 	public AppuntamentoException(String messaggio) {
 		super(messaggio);
 	}
